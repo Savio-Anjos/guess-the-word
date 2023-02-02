5cb498d5e030cd0affce7380429f558187cf1979
unknown <Savioanjos2020@gmail.com>
2023-02-02 20:01:47 -0300

    Criando mudança entre light e dark

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,15 +1,37 @@
 import './StartScreen.scss'
-
-import React from 'react'
+import { BsSun, BsFillMoonFill } from 'react-icons/bs';
+import { useState } from 'react';
 
 const StartScrenn = () => {
-  return (
-  <div className='container'>
-     <h1>Guess The Word</h1>
-     <p>Clique no botão abaixo e comece jogar!</p>
+    const [theme, setTheme] = useState(true)
 
+    const handleTheme = () => {
+        if(theme === true) {
+            setTheme(false)
+        } else {
+            setTheme(true)
+        }
+        
+    }
+  return (
+    <>
+    <div className={`theme ${theme ? 'light' : 'dark'}`}>
+   
+  <div className='container'> 
+     <h1>Guess The Word </h1>  
+     <p>Clique no botão abaixo e comece jogar!</p>
      <button>Começar o jogo</button>
+     <h3>Tema: </h3>
+     {theme ?
+     <span onClick={handleTheme}><BsFillMoonFill size={50} /></span>
+     : <span onClick={handleTheme}> <BsSun size={50} color="#FFF"/></span> 
+     
+     }
+     
   </div>
+  </div> 
+  
+  </>
   )
 }
 
