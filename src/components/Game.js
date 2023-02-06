57d1232274c490eba2dd4b801bb00c6583e5e6da
unknown <Savioanjos2020@gmail.com>
2023-02-06 11:44:23 -0300

    Criando estrutura do game

--- src/components/Game.js
+++ src/components/Game.js
@@ -6,9 +6,35 @@ const Game = ({ handleTheme, theme, verifyLetter}) => {
   return (
 
     <div className={`theme ${theme ? 'light' : 'dark'}`}>
-    <div>
-        <h1>Game</h1>
-        <button className="button" onClick={verifyLetter}>Finalizar Jogo</button>
+    <div className="game">
+      <p className="points">
+          <span>Pontuação: 000</span>
+      </p>
+      <h1>Advinhe a palavra:</h1>
+      <h3 className="tip">
+        Dica sobre a palavra: <span className="category">Fruta</span>
+        <p>Você ainda tem 3 tentativa(s)</p>
+      </h3>
+     
+      <div className="wordContainer">
+        <span className="letter">A</span>
+        <span className="blankSquare"></span>
+      </div>
+
+      <div className="letterContainer">
+        <p>Tente advinhar uma letra da palavra: </p>
+        <form>
+          <input type="text" name="letter" maxLength="1" required/>
+          <button>Jogar!</button>
+        </form>
+      </div>
+      <div className="wrongLettersContainer">
+        <p>Letras já utilizadas:</p>
+        <span> a,</span>
+        <span> b,</span>
+      </div>
+      
+        
     </div>
      <Theme  theme={theme} handleTheme={handleTheme}/>
      </div>
@@ -16,4 +42,4 @@ const Game = ({ handleTheme, theme, verifyLetter}) => {
   )
 }
 
-export default Game
+export default Game;
