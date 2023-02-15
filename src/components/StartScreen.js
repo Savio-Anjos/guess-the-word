4f5e71bc2b0b5cf11037693c5a56e39f5b7a1903
unknown <Savioanjos2020@gmail.com>
2023-02-14 21:19:55 -0300

    Adicionando animação na tela inicial

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,8 +1,25 @@
+import { useState } from 'react';
 import './StartScreen.scss'
 
 import Theme from './Theme';
 
+import Lottie from 'react-lottie';
+import animationStart from './startAnimation.json';
+
 const StartScrenn = ({handleTheme, theme, startGame }) => {
+
+  const [animationState, setAnimationState] = useState({
+    isStopped: false, isPaused: false
+  });
+
+  const optionsStart = {
+    loop: true,
+    autoplay: true, 
+    animationData: animationStart,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  }
    
   return (
     <>
@@ -10,6 +27,13 @@ const StartScrenn = ({handleTheme, theme, startGame }) => {
    
   <div className='container'> 
      <h1>Guess The Word </h1>  
+     <Lottie 
+              options={optionsStart}
+              height={200}
+              width={200}
+              isStopped={animationState.isStopped}
+              isPaused={animationState.isPaused}  
+              />
      <p>Clique no botão abaixo e comece jogar!</p>
      <button className='button' onClick={startGame}>Começar o jogo</button>
      
